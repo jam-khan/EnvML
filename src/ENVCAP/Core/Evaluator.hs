846f0f7a4e8d6a8bdf56889b44529a1245937d5a
jam-khan <jamkhan@connect.hku.hk>
2025-11-15 19:29:17 +0800

    Clean up of semantics

--- src/ENVCAP/Core/Evaluator.hs
+++ src/ENVCAP/Core/Evaluator.hs
@@ -1,9 +1,90 @@
-{-# LANGUAGE LambdaCase #-}
-
 module ENVCAP.Core.Evaluator where
-
 import ENVCAP.Syntax
-import ENVCAP.Utils (arithOp, compareOp)
+import Control.Monad (guard)
+
+eval :: Value -> CoreTm -> Maybe Value
+eval env Ctx            = pure env
+eval _ Unit             = pure VUnit
+eval _ (Lit n)          = pure $ VInt n
+eval _ (EBool b)        = pure $ VBool b
+eval _ (EString s)      = pure $ VString s
+eval env (Lam t e)      = pure $ VClos env (Lam t e)
+eval env (Proj e n)     = do  
+    v <- eval env e
+    lookupv v n
+eval env (Clos e1 e2)   = do
+    v1 <- eval env e1
+    return $ VClos v1 e2
+eval env (Rec s e)      = do
+    v <- eval env e
+    return $ VRcd s v
+eval env (RProj e s)    = do
+    v1 <- eval env e
+    rlookupv v1 s
+eval env (App e1 e2)    = do
+    (VClos v lambda) <- eval env e1
+    v2               <- eval env e2
+    apply v lambda v2
+    where
+        apply :: Value -> CoreTm -> Value -> Maybe Value
+        apply env' (Lam _ e) arg =
+            eval (VMrg env' arg) e
+        apply env' fix@(Fix _ (Lam _ e')) arg =
+            eval (VMrg (VMrg env' (VClos env' fix)) arg) e'
+        apply _ _ _ = Nothing
+eval env (Mrg e1 e2)    = do
+    v1 <- eval env e1 
+    v2 <- eval (VMrg env v1) e2
+    return $ VMrg v1 v2
+eval env (Box e1 e2)    = do
+    v1 <- eval env e1
+    eval v1 e2
+eval env (Fix ty e)     = pure $ VClos env (Fix ty e)
+eval env (Tag tm ty)    = do
+    v <- eval env tm
+    return $ VTag v ty
+eval env (Case tm cases) = do
+    (VTag (VRcd l v) _) <- eval env tm
+    tm' <- getCase l cases
+    eval (mergeEnvTuple env v) tm'
+    where
+        mergeEnvTuple env' (VMrg v1 v2) = VMrg (mergeEnvTuple env' v1) v2
+        mergeEnvTuple env' v' = VMrg env' v'
+eval env (If cond e1 e2) = do
+    VBool b <- eval env cond
+    eval env (if b then e1 else e2)
+eval env (BinOp (Arith op) e1 e2) = do
+    (VInt v1) <- eval env e1
+    (VInt v2) <- eval env e2
+    VInt <$> arithOp op v1 v2   
+eval env (BinOp (Comp op) e1 e2)    = do
+    v1 <- eval env e1
+    v2 <- eval env e2
+    pure $ VBool (compareOp op v1 v2)
+eval env (BinOp (Logic op) e1 e2)   = do
+    (VBool b1) <- eval env e1
+    (VBool b2) <- eval env e2
+    pure $ VBool (solve op b1 b2)
+    where
+        solve And v1 v2 = v1 && v2
+        solve Or v1 v2  = v1 || v2 
+eval env (UnOp Not e1)  = do
+    (VBool b ) <- eval env e1
+    return $ VBool (not b)
+eval env (Cons e1 e2)   = do
+    v1 <- eval env e1
+    v2 <- eval env e2
+    return $ VCons v1 v2
+eval env (LCase e1 e2 e3) = do
+    v1 <- eval env e1
+    solveLCase v1
+    where
+        solveLCase (VNil _ty)    = eval env e2
+        solveLCase (VCons v1 v2) = eval (VMrg (VMrg env v1) v2) e3
+        solveLCase _             = Nothing
+eval _ (Nil ty)      = pure $ VNil ty
+eval _ (CLam _ _)    = error "NOT SUPPORTED CLam"
+eval env (Anno tm _) = eval env tm
 
 lookupv :: Value -> Integer -> Maybe Value
 lookupv (VMrg _ v2) 0 = Just v2
@@ -25,88 +106,30 @@ getCase _ [] = Nothing
 getCase l (((l', _), tm) : xs) =
     if l == l' then Just tm else getCase l xs
 
-eval :: Value -> CoreTm -> Maybe Value
-eval env Ctx = Just env
-eval _ Unit = Just VUnit
-eval _ (Lit n) = Just (VInt n)
-eval _ (EBool b) = VBool <$> Just b
-eval _ (EString s) = VString <$> Just s
-eval env (Lam t e) = Just (VClos env (Lam t e))
-eval env (Proj e n) = eval env e >>= \v1 -> lookupv v1 n
-eval env (Clos e1 e2) = eval env e1 >>= \v1 -> return $ VClos v1 e2
-eval env (Rec s e) = eval env e >>= \v -> return $ VRcd s v
-eval env (RProj e s) = eval env e >>= \v1 -> rlookupv v1 s
-eval env (App e1 e2) =
-    eval env e1 >>= \case
-        (VClos v (Lam _ e)) ->
-            eval env e2 >>= \v2 -> eval (VMrg v v2) e
-        (VClos v (Fix t (Lam t' e'))) ->
-            eval env e2 >>= \v2 ->
-                eval (VMrg (VMrg v (VClos v (Fix t (Lam t' e')))) v2) e'
-        _ -> Nothing
-eval env (Mrg e1 e2) = eval env e1 >>= \v1 -> eval (VMrg env v1) e2 >>= \v2 -> return $ VMrg v1 v2
-eval env (Box e1 e2) = eval env e1 >>= \v1 -> eval v1 e2
-eval env (If cond e1 e2) =
-    eval env cond
-        >>= \case
-            VBool True -> eval env e1
-            VBool False -> eval env e2
-            _ -> Nothing
-eval env (Fix ty e) = Just (VClos env (Fix ty e))
-eval env (Tag tm ty) =
-    eval env tm
-        >>= \v -> return $ VTag v ty
-eval env (Case tm cases) =
-    eval env tm
-        >>= \case
-            (VTag (VRcd l v) _) ->
-                case getCase l cases of
-                    Just tm' -> eval (mergeEnvTuple env v) tm'
-                    Nothing -> Nothing
-              where
-                mergeEnvTuple env' v' =
-                    case v' of
-                        VMrg v1 v2 -> VMrg (mergeEnvTuple env' v1) v2
-                        _ -> VMrg env' v'
-            _ -> Nothing
-eval env (BinOp (Arith op) e1 e2) =
-    eval env e1
-        >>= \case
-            (VInt v1) ->
-                eval env e2
-                    >>= \case
-                        (VInt v2) ->
-                            VInt <$> arithOp op v1 v2
-                        _ -> Nothing
-            _ -> Nothing
-eval env (BinOp (Comp op) e1 e2) =
-    eval env e1
-        >>= \v1 ->
-            eval env e2
-                >>= \v2 -> return $ VBool (compareOp op v1 v2)
-eval env (BinOp (Logic op) e1 e2) =
-    eval env e1
-        >>= \case
-            (VBool b1) ->
-                eval env e2
-                    >>= \case
-                        (VBool b2) ->
-                            case op of
-                                And -> return $ VBool (b1 && b2)
-                                Or -> return $ VBool (b1 || b2)
-                        _ -> Nothing
-            _ -> Nothing
-eval env (UnOp Not e1) = case eval env e1 of
-    Just (VBool b) -> Just (VBool (not b))
-    _ -> Nothing
-eval env (Nil ty) = Just (VNil ty)
-eval env (Cons e1 e2) =
-    eval env e1 >>= \v1 ->
-        eval env e2 >>= \v2 ->
-            return $ VCons v1 v2
-eval env (LCase e1 e2 e3) = case eval env e1 of
-    Just (VNil ty) -> eval env e2
-    Just (VCons v1 v2) -> eval (VMrg (VMrg env v1) v2) e3
-    _ -> Nothing
-eval _ (CLam _ _) = Nothing
-eval env (Anno tm _) = eval env tm
+arithOp :: ArithOp -> Integer -> Integer -> Maybe Integer
+arithOp Add v1 v2 = pure $ v1 + v2
+arithOp Sub v1 v2 = pure $ v1 - v2
+arithOp Mul v1 v2 = pure $ v1 * v2
+arithOp Div v1 v2 = do
+    guard (v2 /= 0) 
+    return $ v1 `div` v2
+arithOp Mod v1 v2 = do
+    guard (v2 /= 0) 
+    return $ v1 `mod` v2
+
+compareOp :: CompOp -> Value -> Value -> Bool
+compareOp Eql v1 v2     = v1 == v2
+    -- compareWith op v1 v2
+compareOp op (VBool b1) (VBool b2) =
+    compareWith op b1 b2
+compareOp op (VString s1) (VString s2) =
+    compareWith op s1 s2
+compareOp _ _ _ = False
+
+compareWith :: (Ord a) => CompOp -> a -> a -> Bool
+compareWith Eql x y     = x == y
+compareWith Neq x y     = x /= y
+compareWith Lt x y      = x < y
+compareWith Le x y      = x <= y
+compareWith Gt x y      = x > y
+compareWith Ge x y      = x >= y
