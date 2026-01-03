7efb7ed0a5f6688eaac869b01601b61a396c0b67
richie <richiewillynton@gmail.com>
2026-01-04 01:38:15 +0700

    pretty printer tests

--- src/EnvML/Print.hs
+++ src/EnvML/Print.hs
@@ -29,7 +29,7 @@ expPrec e = case e of
   TLam {} -> 2
   App _ _ -> 3
   TApp _ _ -> 3
-  RProj _ _ -> 4
+  RProj _ _ -> 3
   Lit _ -> 5
   Var _ -> 5
   Rec _ _ -> 5
@@ -52,21 +52,18 @@ stringOfTyEnv (b : bs) = stringOfTyBind b ++ ", " ++ stringOfTyEnv bs
 
 stringOfEnvBind :: (Name, EnvE) -> String
 stringOfEnvBind (n, ExpE e) = n ++ " = " ++ stringOfExp e
-stringOfEnvBind (n, TypE t) = n ++ " : " ++ stringOfTyp t
+stringOfEnvBind (n, TypE t) = "type " ++ n ++ " = " ++ stringOfTyp t
 
 stringOfEnv :: Env -> String
 stringOfEnv [] = ""
 stringOfEnv [b] = stringOfEnvBind b
 stringOfEnv (b : bs) = stringOfEnvBind b ++ ", " ++ stringOfEnv bs
 
-rShowBinds :: TyEnv -> String
-rShowBinds = stringOfTyEnv . reverse
-
 stringOfIntfE :: IntfE -> String
-stringOfIntfE (TyDef n t) = "type " ++ show n ++ " = " ++ stringOfTyp t
-stringOfIntfE (ValDecl n t) = "val " ++ show n  ++ " :  "  ++ stringOfTyp t
-stringOfIntfE (ModDecl n1 n2) = "module " ++ show n1  ++ " :  "  ++ show n2
-stringOfIntfE (SigDecl n mt) = "module type " ++ show n  ++ " = "  ++ stringOfModuleTyp mt
+stringOfIntfE (TyDef n t) = "type " ++ n ++ " = " ++ stringOfTyp t
+stringOfIntfE (ValDecl n t) = "val " ++ n  ++ " :  "  ++ stringOfTyp t
+stringOfIntfE (ModDecl n1 n2) = "module " ++ n1  ++ " :  "  ++ n2
+stringOfIntfE (SigDecl n mt) = "module type " ++ n  ++ " = "  ++ stringOfModuleTyp mt
 
 stringOfIntf :: Intf -> String
 stringOfIntf [] = ""
@@ -77,7 +74,7 @@ stringOfModuleTyp :: ModuleTyp -> String
 stringOfModuleTyp (TyArrowM t m) = 
     let sT = stringOfTyp t
         sM = stringOfModuleTyp m
-     in "(" ++ sT ++ " -> " ++ sM ++ ")"
+     in "(" ++ sT ++ " ->m " ++ sM ++ ")"
 stringOfModuleTyp (TySig mt) = 
     let sIntf = stringOfIntf mt
      in "sig " ++ sIntf ++ " end"
@@ -86,73 +83,73 @@ stringOfModule :: Module -> String
 stringOfModule (Functor n t m) =
     let sT = stringOfTyp t
         sM = stringOfModule m
-     in "functor (" ++ show n ++ " : " ++ sT ++ ") " ++ sM
+     in "functor (" ++ n ++ " : " ++ sT ++ ") " ++ sM
 stringOfModule (Struct env) = 
     let sEnv = stringOfEnv env
      in "struct " ++ sEnv ++ " end"
 stringOfModule (MApp m1 m2) = 
     let sM1 = stringOfModule m1
         sM2 = stringOfModule m2
-     in "( " ++ sM1 ++ ") ( " ++ sM2 ++ " )"
+     in "(" ++ sM1 ++ ") (" ++ sM2 ++ ")"
 stringOfModule (MLink m1 m2) = 
     let sM1 = stringOfModule m1
         sM2 = stringOfModule m2
-     in "( " ++ sM1 ++ " ) |><| ( " ++ sM2 ++ " )"
+     in "(" ++ sM1 ++ ") |><| (" ++ sM2 ++ ")"
 
 
 stringOfTyp :: Typ -> String
-stringOfTyp (TyLit n) = show n
-stringOfTyp (TyVar s) = show s
+stringOfTyp (TyLit l) = show l
+stringOfTyp (TyVar s) = s
 stringOfTyp (TyArr t1 t2) =
   let s1 = parensIf (typPrec t1 <= typPrec (TyArr t1 t2)) (stringOfTyp t1)
       s2 = stringOfTyp t2
    in s1 ++ " -> " ++ s2
 stringOfTyp (TyAll x t) =
   let s = parensIf (typPrec t < typPrec (TyAll x t)) (stringOfTyp t)
-   in "forall " ++ show x ++ ". " ++ s
+   in "forall " ++ x ++ ". " ++ s
 stringOfTyp (TyBoxT bs t) =
-  let sBinds = rShowBinds bs
+  let sBinds = stringOfTyEnv bs
       sTyp = parensIf (typPrec t < typPrec (TyBoxT bs t)) (stringOfTyp t)
    in "[" ++ sBinds ++ "] ===> " ++ sTyp
 stringOfTyp (TySubstT t1 x t2) =
   let s1 = stringOfTyp t1
       s2 = parensIf (typPrec t2 < typPrec (TySubstT t1 x t2)) (stringOfTyp t2)
-   in s1 ++ "[" ++ show x ++ ":=" ++ s2 ++ "]"
-stringOfTyp (TyEnvt bs) = "[" ++ rShowBinds bs ++ "]"
+   in s1 ++ "[" ++ x ++ ":=" ++ s2 ++ "]"
+stringOfTyp (TyEnvt bs) = "[" ++ stringOfTyEnv bs ++ "]"
 stringOfTyp (TyRcd label t) = "{" ++ label ++ " : " ++ stringOfTyp t ++ "}"
 stringOfTyp (TyModule mt) = stringOfModuleTyp mt
 
 stringOfExp :: Exp -> String
-stringOfExp (Lit n) = show n
-stringOfExp (Var n) = show n
+stringOfExp (Lit l) = show l
+stringOfExp (Var n) = n
 stringOfExp (Lam n t e) =
   let sT = stringOfTyp t
       sE = stringOfExp e
-   in "fun (" ++ show n ++ " : " ++ sT ++ ") -> " ++ sE
+   in "fun (" ++ n ++ ": " ++ sT ++ ") -> " ++ sE
 stringOfExp (Box env e) =
   let sCets = stringOfEnv env
       sE = parensIf (expPrec e < expPrec (Box env e)) (stringOfExp e)
-   in "box [" ++ sCets ++ "] |> in " ++ sE
+   in "box [" ++ sCets ++ "] in " ++ sE
 stringOfExp (App e1 e2) =
   let s1 = parensIf (expPrec e1 < expPrec (App e1 e2)) (stringOfExp e1)
       s2 = stringOfExp e2
-   in s1 ++ " ( " ++ s2 ++ " )"
+   in s1 ++ "(" ++ s2 ++ ")"
 stringOfExp (TLam n e) =
     let sE = stringOfExp e
-     in "fun type " ++ show n ++ " -> " ++ sE
+     in "fun type " ++ n ++ " -> " ++ sE
 stringOfExp (Clos cetList n t e) =
   let sCets = stringOfEnv cetList
       sT = stringOfTyp t
       sE = parensIf (expPrec e < expPrec (Clos cetList n t e)) (stringOfExp e)
-   in "clos [ " ++ sCets ++ " ] (" ++ show n ++ " : " ++ sT ++ ") -> " ++ sE
+   in "clos [" ++ sCets ++ "] (" ++ n ++ ": " ++ sT ++ ") -> " ++ sE
 stringOfExp (TClos cetList n e) =
   let sCets = stringOfEnv cetList
       sE = parensIf (expPrec e < expPrec (TClos cetList n e)) (stringOfExp e)
-   in "clos [ " ++ sCets ++ " ] type " ++ show n ++ "-> " ++ sE
+   in "clos [" ++ sCets ++ "] type " ++ n ++ "-> " ++ sE
 stringOfExp (TApp e t) =
   let sE = parensIf (expPrec e < expPrec (TApp e t)) (stringOfExp e)
       sT = stringOfTyp t
-   in sE ++ "< " ++ sT ++ " >"
+   in sE ++ "<" ++ sT ++ ">"
 stringOfExp (FEnv env) =
   let sEnv = stringOfEnv env
    in "[" ++ sEnv ++ "]"
---
+++ test/EnvML/PrintExpSpec.hs
@@ -0,0 +1,128 @@
+module EnvML.PrintExpSpec (spec) where
+
+import Test.Hspec
+import EnvML.Syntax
+import EnvML.Print (stringOfExp)
+
+-- Shorthands
+int :: Typ
+int = TyLit TyInt
+
+x, y, z :: Exp
+x = Var "x"
+y = Var "y"
+z = Var "z"
+
+prettyExpTests :: [(Exp, String)]
+prettyExpTests =
+  [ -- Literals
+    (Lit (LitInt 0),  "0")
+  , (Lit (LitInt 42), "42")
+
+    -- Variables
+  , (Var "x", "x")
+
+    -- Application
+  , (App x y, "x(y)")
+  , (App (App x y) z, "x(y)(z)")
+  , (App x (App y z), "x(y(z))")
+
+    -- Lambdas
+  , (Lam "x" int x, "fun (x: int) -> x")
+  , (Lam "x" int (App x y), "fun (x: int) -> x(y)")
+  , (App (Lam "x" int x) y, "(fun (x: int) -> x)(y)")
+
+    -- Type abstraction
+  , (TLam "a" x, "fun type a -> x")
+  , (Lam "x" (TyVar "a") (TLam "a" x),
+      "fun (x: a) -> fun type a -> x")
+
+    -- Type application
+  , (TApp x int, "x<int>")
+  , (TApp (App x y) int, "x(y)<int>")
+  , (App (TApp x int) y, "x<int>(y)")
+
+    -- Annotations
+  , (Anno x int, "x :: int")
+  , (Anno (App x y) int, "x(y) :: int")
+  , (App (Anno x int) y, "(x :: int)(y)")
+
+    -- Boxes
+  , (Box [] x, "box [] in x")
+  , (Box [] (App x y), "box [] in x(y)")
+  , (App (Box [] x) y, "(box [] in x)(y)")
+
+    -- Closures
+  , (Clos [] "x" int x, "clos [] (x: int) -> x")
+  , (App (Clos [] "x" int x) y, "(clos [] (x: int) -> x)(y)")
+  , (Clos [("x", ExpE x)] "y" int (App x y),
+      "clos [x = x] (y: int) -> x(y)")
+
+    -- Records
+  , (Rec "x" (Lit (LitInt 42)), "{x = 42}")
+  , (RProj x "x", "x.x")
+  , (RProj (App x y) "x", "x(y).x")
+
+    -- Environments
+  , (FEnv [], "[]")
+  , (FEnv [("x", ExpE x)], "[x = x]")
+  , (FEnv [("t", TypE int), ("x", ExpE (Lit (LitInt 42)))],
+      "[type t = int, x = 42]")
+
+    -- Precedence tests
+  , (App (RProj x "x") y, "x.x(y)")
+  , (Anno (RProj x "x") int, "x.x :: int")
+
+    -- Module expressions
+  , (ModE (Struct []),
+     "struct  end")
+
+  , (ModE (Struct [("x", ExpE (Lit (LitInt 1)))]),
+     "struct x = 1 end")
+
+  , (ModE (Functor "X" (TyVar "A") (Struct [])),
+     "functor (X : A) struct  end")
+
+  , (ModE
+        (MApp
+          (Functor "X" (TyVar "A") (Struct []))
+          (Struct [])),
+     "(functor (X : A) struct  end) (struct  end)")
+
+  , (ModE
+        (MLink
+          (Struct [])
+          (Struct [])),
+     "(struct  end) |><| (struct  end)")
+
+  , (RProj (ModE (Struct [])) "x",
+     "struct  end.x")
+
+  , (Anno (ModE (Struct [])) (TyModule (TySig [])),
+     "struct  end :: sig  end")
+
+    -- Precedence tests
+  , (App (Lam "x" int x) (Lam "y" int y),
+     "(fun (x: int) -> x)(fun (y: int) -> y)")
+
+  , (App (Box [] (Lam "x" int x)) y,
+     "(box [] in fun (x: int) -> x)(y)")
+
+  , (RProj (TApp x (TyVar "A")) "l",
+     "x<A>.l")
+
+  , (Anno (App (TApp x int) y) int,
+     "x<int>(y) :: int")
+
+  , (App (Anno x int) (Anno y int),
+     "(x :: int)(y :: int)")
+  ]
+
+spec :: Spec
+spec =
+  describe "Pretty.stringOfExp" $
+    mapM_ mkTest prettyExpTests
+  where
+    mkTest (e, expected) =
+      it ("prints " ++ stringOfExp e) $
+        stringOfExp e `shouldBe` expected
