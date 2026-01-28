ccb3b1e41bcc741815dae5914ca4714c22bdd134
jam-khan <jamkhan@connect.hku.hk>
2026-01-29 02:37:04 +0800

    Minor clean up

--- src/CoreForAll/Repl.hs
+++ src/CoreForAll/Repl.hs
@@ -13,36 +13,35 @@ import CoreForAll.Check (infer)
 
 main :: IO ()
 main = do
-    putStrLn "Welcome to the Calculus REPL"
-    putStrLn "Commands: :t (test suite), :q (quit)"
-    repl
+  putStrLn "Welcome to the Calculus REPL"
+  putStrLn "Commands: :t (test suite), :q (quit)"
+  repl
 
 repl :: IO ()
 repl = do
-    putStr "λ> "
-    hFlush stdout
-    input <- getLine
-    case input of
-        ":q" -> putStrLn "Goodbye!"
-        _ | null input -> repl
-        _ -> do
-            handleInput input
-            repl
+  putStr "λ> "
+  hFlush stdout
+  input <- getLine
+  case input of
+    ":q" -> putStrLn "Goodbye!"
+    _ | null input -> repl
+    _ -> do
+      handleInput input
+      repl
 
 handleInput :: String -> IO ()
 handleInput str = do
-    -- 1. Parse
-    exprRes <- try (return $! parseExp (lexer str)) :: IO (Either SomeException Exp)
-    case exprRes of
-        Left err -> putStrLn $ "Parse Error: " ++ show err
-        Right e  -> do
-            -- 2. Type Check (Inference)
-            case infer [] e of
-                Nothing -> putStrLn "Type Error: Could not infer type"
-                Just t  -> do
-                    putStrLn $ "Type   : " ++ stringOfTyp t
-                    
-                    -- 3. Evaluate
-                    case eval [] e of
-                        Nothing -> putStrLn "Eval Error: Evaluation failed"
-                        Just v  -> putStrLn $ "Result : " ++ stringOfExp v
+  -- 1. Parse
+  exprRes <- try (return $! parseExp (lexer str)) :: IO (Either SomeException Exp)
+  case exprRes of
+    Left err -> putStrLn $ "Parse Error: " ++ show err
+    Right e  -> do
+      -- 2. Type Check (Inference)
+      case infer [] e of
+        Nothing -> putStrLn "Type Error: Could not infer type"
+        Just t  -> do
+          putStrLn $ "Type   : " ++ stringOfTyp t
+          -- 3. Evaluate
+          case eval [] e of
+            Nothing -> putStrLn "Eval Error: Evaluation failed"
+            Just v  -> putStrLn $ "Result : " ++ stringOfExp v
